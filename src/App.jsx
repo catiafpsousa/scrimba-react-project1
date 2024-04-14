58d2315a451db59ddfe6db3e36efe672deeb564f
catiafpsousa <catiafpsousa@gmail.com>
2024-04-14 23:43:49 +0100

    css improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import photo from './assets/photo.jpg'
 import './App.css'
 import './components/Header.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Profile from './components/Profile.jsx'
+
 
 function App() {
   return (
